8179efdc94a6c7521d6b07cdea7105abc3be5001
Sumit Saurav <impossibleshadowstorm@gmail.com>
2025-03-15 18:39:06 +0530

    feat: added landing page..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from '@/components/Hero';
 import Testimonials from '@/components/Testimonials';
-import Pricing from '@/components/Pricing/Pricing';
 import FAQ from '@/components/FAQ';
 import Logos from '@/components/Logos';
 import Benefits from '@/components/Benefits/Benefits';
@@ -17,13 +16,13 @@ const HomePage: React.FC = () => {
       <Container>
         <Benefits />
 
-        <Section
-          id='pricing'
-          title='Pricing'
-          description='Simple, transparent pricing. No surprises.'
+        {/* <Section
+          id="pricing"
+          title="Pricing"
+          description="Simple, transparent pricing. No surprises."
         >
           <Pricing />
-        </Section>
+        </Section> */}
 
         <Section
           id='testimonials'
