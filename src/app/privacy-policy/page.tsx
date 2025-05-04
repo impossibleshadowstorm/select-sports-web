a657be6ab2cbaa09650377cf8d6ff04521e1fb12
Sumit Saurav <impossibleshadowstorm@gmail.com>
2025-05-04 17:15:06 +0530

    minor fixes..

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,13 +1,5 @@
 import { Metadata } from 'next';
-import SignInViewPage from '@/features/auth/components/sigin-view';
 import PrivacyPolicyPage from '@/features/privacy-policy/privacy-policy';
-// import dynamic from "next/dynamic";
-// import { Suspense } from 'react';
-
-// const SignInViewPage = dynamic(
-//   () => import("@/features/auth/components/sigin-view"),
-//   { ssr: false }
-// );
 
 export const metadata: Metadata = {
   title: 'Privacy Policy | Select Sports',
@@ -15,9 +7,5 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-  return (
-    // <Suspense fallback={<div>Loading...</div>}>
-    <PrivacyPolicyPage />
-    // </Suspense>
-  );
+  return <PrivacyPolicyPage />;
 }
